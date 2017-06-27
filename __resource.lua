2070b1ed7fdaeb45355782b021756481af673a7d
Asgarth1977 <m.baegen@live.co.uk>
2017-06-27 17:58:05 +0100

    Original Files FiveM
    This first commit, will be the original files that you can download from
    FiveM's website.
    From here we will be changing, developing and editting to make our
    server run as we would like it to see.

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+-- Manifest
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+-- Information
+-- v1 couchDB Banking System
+
+client_scripts {
+	'client/client.lua'
+}
+
+server_scripts {
+	'server/server.lua'
+}
+
---
+++ database/db_connection.lua
@@ -0,0 +1,9 @@
+db = {
+
+	driver = "couchdb" ,
+	
+	-- Connection Info
+	couchDB_ip 		= "127.0.0.1",
+	couchDB_port 	= 5984,
+	couchDB_auth	= "username:password"
+}
